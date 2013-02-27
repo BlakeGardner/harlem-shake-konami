39bcdb585b230addc2944d97944013e78843549c
Blake Gardner <blakegardner@cox.net>
2013-02-27 11:27:39 -0600

    Load and bootstrap jQuery if needed

---
+++ shake.js
@@ -0,0 +1,23 @@
+
+var harlem = {
+	shake: function(){
+		$(document).ready(function(){
+			console.log('ready to do some fun stuff');
+			$(document).keydown(function(e){
+				console.log(e.keyCode);
+			});
+		});
+	}
+};
+
+
+if (typeof jQuery == 'undefined') {
+	jq = document.createElement('script');
+	jq.type = 'text/javascript';
+	jq.async = true;
+	jq.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+	jq.onload=harlem.shake;
+	document.getElementsByTagName('head')[0].appendChild(jq);
+} else {
+	harlem.shake();
+}
