3772ad6aad11697cca727ef91f9edfa677d14712
Blake Gardner <blakegardner@cox.net>
2013-02-27 20:37:09 -0600

    Trigger the shake when the Konami code is entered

--- shake.js
+++ shake.js
@@ -1,11 +1,18 @@
 
+konami_code = '38,38,40,40,37,39,37,39,66,65';
+key_stack = new Array();
+
 var harlem = {
 	shake: function(){
 		$(document).ready(function(){
 			console.log('ready to do some fun stuff');
 			$(document).keydown(function(e){
-				console.log(e.keyCode);
-				$('head').append('<script type="text/javascript" src="http://pastebin.com/raw.php?i=ipHpLuEz"></script>');
+				key_stack.push(e.keyCode);
+				//console.log(key_stack.join());
+				//console.log(konami_code);
+				if(key_stack.join() == konami_code) {
+					$('head').append('<script type="text/javascript" src="http://pastebin.com/raw.php?i=ipHpLuEz"></script>');
+				}
 			});
 		});
 	}
